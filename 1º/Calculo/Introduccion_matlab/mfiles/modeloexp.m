9db06ce15422a4ea3e6c3ca08818a4078c18ccfc
Marcos Loro <marcoslorcar03@gmail.com>
2026-06-18 20:42:14 +0200

    Removed matbal introduction

--- 1º/Calculo/Introduccion_matlab/mfiles/modeloexp.m
+++
@@ -1,8 +0,0 @@
-function sse=modeloexp(parametro,T,fd)
-gamma=parametro(1);
-alfa=parametro(2);
-beta=parametro(3);
-ajustemodelo=gamma-alfa.*exp(-beta*T);
-errorvector=ajustemodelo-fd;
-sse=sum(errorvector.^2);
-end
