1fe1280d154af01fdc2d5a4c03434054c0fcfb50
Marcos Loro <123680262+MarcosLorCar@users.noreply.github.com>
2026-06-26 19:37:06 +0200

    P2 S1

--- 2º/Methodology/P2_Sol.java
+++ 2º/Methodology/P2_Sol.java
@@ -38,9 +38,19 @@ public class P2_Sol {
         int resOther = netDamageIterative(records, other);
         end = System.nanoTime();
         System.out.println("Net damage Trainer " + trainer + " (Iterative):     " + res + "  (Time: " + (end - start) + " ns)");
+        start = System.nanoTime();
+        resOther = netDamageDivideAndConquer(records, trainer, 0, records.length - 1);
+        end = System.nanoTime();
+        System.out.println("Net damage Trainer " + trainer + " (Divide and Conquer): " + resOther + "  (Time: " + (end - start) + " ns)");
         System.out.println();
-        char winner = res > resOther ? trainer : other;
+        char winner = calculateWinner(records);
         System.out.println("The winner is Trainer " + winner + "!");
+    }
+
+    private static char calculateWinner(int[] records) {
+        int damageA = netDamageDivideAndConquer(records, 'A', 0, records.length - 1);
+        int damageB = netDamageDivideAndConquer(records, 'B', 0, records.length - 1);
+        return damageA > damageB ? 'A' : 'B';
     }
 
     private static int netDamageIterative(int[] records, char trainer) {
@@ -50,5 +60,21 @@ public class P2_Sol {
             if (record * m > 0) total += record * m;
         }
         return total;
+    }
+
+    private static int netDamageDivideAndConquer(int[] records, char trainer, int start, int end) {
+        if (start == end) { // base case
+            int record = records[start];
+            int m = trainer == 'A' ? 1 : -1;
+            return Math.max(record * m, 0);
+        } else { // recursive case
+            int mid = (start + end) / 2;
+            // conquer part 1
+            int left = netDamageDivideAndConquer(records, trainer, start, mid);
+            // conquer part 2
+            int right = netDamageDivideAndConquer(records, trainer, mid + 1, end);
+            // combine results
+            return left + right;
+        }
     }
 }
